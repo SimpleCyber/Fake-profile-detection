2ed1729126b4931255bb2086df62ca9291042d6e
Satyam Yadav <satyamok03@gmail.com>
2024-12-29 13:25:13 +0530

    modifed easy to navigate templates and static files

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@
 from flask import Flask
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static", template_folder="templates")
 CORS(app)
 
 from app import routes
